171ca6469bcce0ab14ef820e42a37b92cecfc708
Milena Zingerman <Qunzy13@gmail.com>
2022-07-27 20:50:30 +0300

    Delete Client/online_shop directory

--- Client/online_shop/src/utils/Alert.js
+++
@@ -1,9 +0,0 @@
-import Swal from 'sweetalert2';
-
-export const fireAlert = ( message, text = false, isError = false ) => {
-    Swal.fire({
-        icon: isError ? 'error' : 'success',
-        title: message, 
-        text: text ? text : null,
-    });
-};
